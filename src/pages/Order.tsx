7005123c615551d8a0424e48f87270781748d2ca
parsashayegan613-tech <parsashayegan613@gmail.com>
2026-02-25 12:07:38 -0700

    Show all countries in dropdown

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -490,13 +490,11 @@ const Order = () => {
                           <SelectValue placeholder="Select country" />
                         </SelectTrigger>
                         <SelectContent>
-                          {Country.getAllCountries()
-                            .filter((c) => ["CA", "US"].includes(c.isoCode))
-                            .map((country) => (
-                              <SelectItem key={country.isoCode} value={country.isoCode}>
-                                {country.name}
-                              </SelectItem>
-                            ))}
+                          {Country.getAllCountries().map((country) => (
+                            <SelectItem key={country.isoCode} value={country.isoCode}>
+                              {country.name}
+                            </SelectItem>
+                          ))}
                         </SelectContent>
                       </Select>
                     </div>
